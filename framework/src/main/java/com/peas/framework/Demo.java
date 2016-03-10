7d1a1ff3d4c9c3a53188e642393ccbe47d5d7808
Lijq <675517008@qq.com>
2016-03-10 17:53:52 +0800

    Demo

---
+++ framework/src/main/java/com/peas/framework/Demo.java
@@ -0,0 +1,21 @@
+/**
+ * 版权所有 @Lijq / ly_0199@163.com
+ */
+package com.peas.framework;
+
+/**
+ * @author Lijq 2016年3月10日下午5:53:21
+ * @since version 1.0
+ */
+public class Demo {
+	
+	 public static void main(String[] args) {
+		
+		 
+		 
+		 System.out.println("demo");
+		 
+		 
+	}
+
+}
